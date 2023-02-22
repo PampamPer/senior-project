b130c878cace0ad0e820665ff847e0d1f05e1fbe
PoraRee <ree_inter5@hotmail.com>
2023-02-23 01:03:39 +0700

    table footer and change page table

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { styled } from "@mui/material/styles";
+import { useEffect, useState, useContext } from "react";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
@@ -7,6 +8,9 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import TablePaginationActions from "./TablePagination";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
 
 const ignoreColumn = ["semester", "id", "semesterId"];
 const callback = (key) => !ignoreColumn.includes(key);
@@ -51,12 +55,31 @@ const timelines = [
     "2023-04-01",
     "อาจารย์ผู้ประสานงานรายวิชาอัปโหลดไฟล์ สป. 3 ที่ภาคส่งออกและที่คณะอนุมัติ"
   ),
+  createData(6, 2, "2565/2", "2023-04-01", "6"),
+  createData(7, 2, "2565/2", "2023-04-01", "7"),
+  createData(8, 2, "2565/2", "2023-04-01", "8"),
+  createData(9, 2, "2565/2", "2023-04-01", "9"),
+  createData(10, 2, "2565/2", "2023-04-01", "10"),
 ];
 
 export default function CustomizedTables() {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - timelines.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
   return (
     <TableContainer component={Paper}>
-      {console.log(timelines)}
       <Table sx={{ minWidth: 700 }} aria-label="customized table">
         <TableHead>
           <TableRow>
@@ -68,7 +91,19 @@ export default function CustomizedTables() {
           </TableRow>
         </TableHead>
         <TableBody>
-          {timelines.map((timeline) => (
+          {console.log(
+            timelines.slice(
+              page * rowsPerPage,
+              page * rowsPerPage + rowsPerPage
+            )
+          )}
+          {(rowsPerPage > 0
+            ? timelines.slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage
+              )
+            : timelines
+          ).map((timeline) => (
             <TableRow key={timeline.id}>
               {Object.keys(timeline)
                 .filter(callback)
@@ -77,7 +112,33 @@ export default function CustomizedTables() {
                 ))}
             </TableRow>
           ))}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
+
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+              colSpan={3}
+              count={timelines.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  "aria-label": "rows per page",
+                },
+                native: true,
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
       </Table>
     </TableContainer>
   );
